ebc44f94acd50abe4a3a1631d2548ca9152c5a8d
dseg27 <disegovia4@gmail.com>
2021-12-19 15:22:06 -0700

    Mapped polygon geometry objects.

---
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -0,0 +1,50 @@
+// Add console.log() to check to see if our code is working 
+console.log("working"); 
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps 
+let baseMaps = {
+  "Streets": streets,
+  "Satellite Streets": satelliteStreets
+};
+
+// Create the map object with a center and zoom level 
+let map = L.map("mapid",{
+  center: [43.7, -79.3],
+  zoom: 11,
+  layers: [streets]
+});
+
+// Pass map layers to layer control, add to map 
+L.control.layers(baseMaps).addTo(map);
+
+let torontoHoods = "https://raw.githubusercontent.com/dseg27/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+// Create style for lines 
+let myStyle = {
+  fillColor: "yellow",
+  weight: 1
+}
+
+// Grab data 
+d3.json(torontoHoods).then(function(data) {
+  console.log(data);
+  L.geoJSON(data, {
+      style: myStyle,
+      onEachFeature: function(feature, layer){
+        layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
+      }
+  })
+  .addTo(map);
+});
